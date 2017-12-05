2100d9d6298fde295638f7ee89d63b2d7dec5c91
Rosemary <rdt13@ic.ac.uk>
2017-12-05 09:54:47 +0000

    Code all commented and completed

--- Assignment 3/FvR.py
+++ Assignment 3/FvR.py
@@ -1,18 +1,39 @@
 #!/usr/bin/env python3
 
-import sys
+'''
+Rosemary Teague - 00828351
+Computational Methods 2 - Assignment 3
+'''
+
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.linalg
+import scipy.stats
 
 def ParseFile(filename):
+    '''
+    Reads data from filename and stores values of the dynamical matrix as dynmat
+
+    Parameters
+    -----------
+    filename - the name of the data file to be read
+
+    Returns
+    --------
+    dynmat - a 6x6 dynamical matrix for the interaction between two atoms
+    '''
+
+    #Reads file and saves each line to file_lines
     f = open(str(filename), 'r')
     file_lines = f.readlines()
     f.close()
 
+    #creates empty 6x6 dynamical matrix
     dynmat = np.empty((6, 6))
-    row_counter = 0
-    column_counter = 0
+
+    #for each line containing data, the values of the dynamical matrix are saved
+    #to  dynmat. File of the format that each row of the dynamical matrix is
+    #given in one group with a blank line separating rows.
     for line in file_lines[5:]:
         val = line.split()
         if len(val) != 0:
@@ -24,27 +45,84 @@ def ParseFile(filename):
 
 
 def SavePlot(x,y,x2=None,y2=None,xlabel=None,ylabel=None,plotfilename='plot.png'):
+    '''
+    Creates png image of a plot containing one or two data sets
+
+    Parameters
+    -------------
+    (Required)
+    x - 1st set of x data (fitted curve)
+    y - 1st set of y data (fitted curve)
+
+    (Optional)
+    x2 - 2nd set of x data (experimental points)
+    y2 - 2nd set of y data (experimental points)
+    xlabel - label for horizontal axis
+    ylabel - label for vertical axis
+    plotfilename - name of the image to be saved
+    '''
     plt.plot(x, y, "b-")
+
+    #Plot second set of data points if available
     if x2 is not None and y2 is not None:
         plt.plot(x2,y2,'rx')
+    #Add x-axis label if given
     if xlabel is not None:
         plt.xlabel(xlabel)
+    #Add y-axis label if given
     if ylabel is not None:
         plt.ylabel(ylabel)
+    #Saves plot as png
     plt.savefig(plotfilename)
 
 def Main():
+    '''
+    Caluculates the vibrational energy for 6 given dynamical matrices, each representing
+    a bond-length between 2.0 and 2.5. Plots the correlation between the vibrational
+    energies and the bond length with a linear fit.
 
+    Parameters
+    -----------
+    NA
+
+    Returns
+    ---------
+    slope - the slope of the fitted line
+    intercept - the intercept of the fitted line
+    r_value - the correlation coefficient of the fitted line
+    std_err - Standard error of the estimated gradient.
+
+    Saves a png image of the data points and a linearly fitted curve.
+
+    Uses the scipy.stats.linregress function to produce linear fit.
+
+    Atomic units are used througout
+
+    '''
+
+    #creates x data array of bond lengths in Bohn
     Bond_Lengths=np.linspace(2.0,2.5,6)
+    #Creates list to contain Vibrational Energy values when calculated
     VibEnergy=[]
+    #the names of the files containing the data to be analysed
     files=['ab_r2.0_dm','ab_r2.1_dm','ab_r2.2_dm','ab_r2.3_dm','ab_r2.4_dm','ab_r2.5_dm']
+
+    #for each set of data, store the dynamical matrix, find the eigenvalues, and
+    #determine the vibrational energy from these values.
     for file in files:
         dynmat=ParseFile(file)
         eigvals = scipy.linalg.eigvalsh(dynmat)
-        VibEnergy.append(eigvals.max())
+        VibFreq=np.sqrt(eigvals+0j)
+        VibEnergy.append(0.5*np.real(VibFreq.max()))
 
+    #Determine a linear fit and plot against the data points
+    slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(Bond_Lengths,VibEnergy)
+    SavePlot(Bond_Lengths,intercept+slope*Bond_Lengths,Bond_Lengths,VibEnergy,xlabel='Bond Lengths (Bohr)', ylabel = 'Vibrational Energy (Hartree)',plotfilename='Vibrational Energy')
 
-    SavePlot(Bond_Lengths,VibEnergy,xlabel='Bond Lengths', ylabel = 'Vibrational Energy',plotfilename='Vibrational Energy')
+    #Output value of the slope to the terminal
+    print('slope = ', slope)
+
+    return [slope, intercept, r_value, std_err]
 
 if __name__=='__main__':
     Main()
--- Assignment 3/Morse.py
+++ Assignment 3/Morse.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+
+'''
+Rosemary Teague - 00828351
+Computational Methods 2 - Assignment 3
+'''
+
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,45 +20,87 @@ def Morse(r,De,a,re,E):
     De - well depth (dissociation energy)
     a - width of potential
     re - the equilibrium bond distance
-    b - scaling parameterim
+    E - scaling parameter to account for non-zero behaviour at large R
 
     Returns
     --------
     V(r) - the Morse potential profile of atomic separation
     '''
-    return De*(np.exp(-2*a*(r-re))-2*np.exp(-a*(r-re)))-E
 
-def SavePlot(x,y,x2,y2,xlabel=None,ylabel=None,plotfilename='plot.png'):
+    V = De*(np.exp(-2*a*(r-re))-2*np.exp(-a*(r-re)))-E
+    return V
+
+def SavePlot(x,y,x2=None,y2=None,xlabel=None,ylabel=None,plotfilename='plot.png'):
+    '''
+    Creates png image of a plot containing one or two data sets
+
+    Parameters
+    -------------
+    (Required)
+    x - 1st set of x data (fitted curve)
+    y - 1st set of y data (fitted curve)
+
+    (Optional)
+    x2 - 2nd set of x data (experimental points)
+    y2 - 2nd set of y data (experimental points)
+    xlabel - label for horizontal axis
+    ylabel - label for vertical axis
+    plotfilename - name of the image to be saved
+
+    '''
     plt.plot(x, y, "b-")
+
+    #Plot second set of data points if available
     if x2 is not None and y2 is not None:
         plt.plot(x2,y2,'rx')
+    #Add x-axis label if given
     if xlabel is not None:
         plt.xlabel(xlabel)
+    #Add y-axis label if given
     if ylabel is not None:
         plt.ylabel(ylabel)
+    #Saves plot as png
     plt.savefig(plotfilename)
 
 
 def Main(filename):
+    '''
+    Reads data for the energy and separation of a diatomic molecule and fits the
+    Morse potential to this data.
+
+    Parameters
+    -----------
+    filename - the name of the file containing Energy and Separation data to be readlines
+
+    Returns
+    --------
+    Mp - a list containing the fitted parameters of the potential
+
+    Each of the fitted parameters will be printed to the terminal and a png image
+    of the data and interpolated plot will be saved as 'Morse Potential.png'
+    '''
+
+    #Reads the data from given file and stores Energy and Separation values as seperate lists
     Data=np.loadtxt(str(filename))
     R=Data[:,0]
     E=Data[:,1]
-    print(R[0],E[0])
 
-
+    #Gives initial guesses for the fitting parameters from the data points
     Morse_guess = [E.min(),1,R[np.argmin(E)],E[-1]]
+    #Fits a curve of the form of the Morse Potential to the data points
     Mp, cov = scipy.optimize.curve_fit(Morse, R, E, Morse_guess)
 
+    #Outputs the final fitted parameters to the terminal
     print("De = ", Mp[0])
     print("a = ", Mp[1])
     print("re = ", Mp[2])
     print('E = ', Mp[3])
 
+    #saves a plot of the data points and fitted curve as a png image
     xplt=np.linspace(R.min(),R.max(),100)
-
     SavePlot(xplt,Morse(xplt,*Mp),R,E,xlabel='Atomic Separation',ylabel='Potential',plotfilename='Morse Potential')
 
-    return str(filename)
+    return Mp
 
 if __name__=='__main__':
     Main(sys.argv[1])
