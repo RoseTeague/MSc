28db781aaec6fce3f2312af997269be29f6697b9
Rosemary <rdt13@ic.ac.uk>
2017-12-14 12:13:46 +0000

    Working calculations of displacements and energies

--- Assignment 4/Lennard_Jones.py
+++ Assignment 4/Lennard_Jones.py
@@ -2,6 +2,8 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.fftpack
+import seaborn as sns
 
 def Init(N):
     '''
@@ -20,7 +22,7 @@ def Init(N):
     #Equilibrium separation
     do = 3.9 #Angstrom
     #Random number to determine an initial displacement from equilibrium
-    sigma = np.sqrt(0.01)
+    sigma = np.sqrt(0.001)
     s = np.random.normal(0,sigma,N)
     #s = np.zeros(N)
 
@@ -45,8 +47,11 @@ def Accel(pos):
 
     Returns
     -------
+    a - array, acceleration of each atom
+    V - Total potential energy (eV) for this arrangement
     F - array, the force on each atom as a result of interactions with every other
         atom in the chain
+
     '''
 
     A = 1.209*10**(5)#0.0011639175169510549#
@@ -57,42 +62,64 @@ def Accel(pos):
 
     #Matrix of interactions between each atomic pair
     rmat = np.array([[pos[n]-pos[m] for n in range(N) if n!=m] for m in range(N)])
-    Vmat = A*np.abs(rmat[:,:])**(-12)-B*np.abs(rmat[:,:])**(-6)
-    Fmat = -np.sign(rmat[:,:])*12*np.multiply(Vmat,1/np.abs(rmat))
+    #Lennard-Jones Potential generated between each atomic pair
+    Vmat = A*np.abs(rmat[:,:])**(-12)-B*np.abs(rmat[:,:])**(-6) #
+    #Force between each atomic pair
+    Fmat = -np.sign(rmat[:,:])*12*(A*np.abs(rmat[:,:])**(-13)-0.5*B*np.abs(rmat[:,:])**(-7))
 
+    #Total potential and force for each atom
     V = np.array([np.sum(Vmat[i]) for i in range(N)])
     F = np.array([np.sum(Fmat[i]) for i in range(N)])
 
-    m_to_A = 10e10
-    s_to_ps = 10e12
-    kg_to_eVc = 1/(1.6e-19)#(3e8)**2/(1.6e-19)
+    #Unit conversions
+    e = 1.6e-19
+    A_to_m = 1e-10
+    ps_to_s = 1e-12
 
-    m = 6.63e-26*kg_to_eVc*10**(4)
+    #Alternative units - to SI and back
+    Fev_to_FSI = e/A_to_m #converting a force in eV per Angstrom to kg m s^-2
+    aSI_to_aAps = ps_to_s**2/A_to_m # converting an acceleration from ms^-2 to Angstrom per ps^-2
 
+    m = 6.63e-26/(Fev_to_FSI*aSI_to_aAps) # mass in eV ps^2 per Angstrom^2
     a = F/m
 
-    plt.figure(1)
-    plt.imshow(rmat)
-    plt.colorbar()
-    plt.show()
+    # Figures to illustrate the forces and potentials on each argon atoms
 
-    plt.figure(2)
-    plt.plot(pos,V)
+    # plt.figure(1)
+    # plt.imshow(rmat)
+    # plt.colorbar()
+    # plt.show()
 
-    plt.figure(3)
-    plt.plot(pos,F)
-
-    plt.figure(5)
-    plt.plot(pos,a)
+    # plt.figure(2)
+    # plt.plot(pos,V)
+    #
+    # plt.figure(3)
+    # plt.plot(pos,F)
+    #
+    # plt.figure(5)
+    # plt.plot(pos,a)
 
     #Verification that force contributions are mainly from nearest neighbours
-    plt.figure(4)
-    plt.imshow(Fmat)
-    plt.colorbar()
-    plt.show()
+    # plt.figure(4)
+    # plt.imshow(Fmat)
+    # plt.colorbar()
+    # plt.show()
 
+    # diving the total potential by 2 eliminates double counting from pairs
+    return a,np.sum(V)/2
 
-    return a,2*np.sum(V)/N,rmat,Fmat
+def adjacent_pairs(x):
+    '''
+    Parameters
+    ----------
+    x - array
+        positions of all atoms in chain
+    '''
+    N = len(x)
+    r = [pos[n]-pos[n+1] for n in range(N-1)]
+
+    return r
+
 
 def velocityverlet(N, accel, x0, v0, dt, nt):
     '''Velocity Verlet integration to find x(t) and v(t)
@@ -112,11 +139,15 @@ def velocityverlet(N, accel, x0, v0, dt, nt):
 
     Returns
     -------
-    x : float, array
+    x : float, (N x nt) array
         Position at each timestep.
+    v : float, (N x nt) array
+        Velocity at each timesteps
+    V : float, (nt) array
+        Potential energy at each timestep
     '''
-    #print(dt,nt)
-    # Initialize an empty arrays that will store x at each timestep.
+
+    # Initialize an empty arrays that will store x,v and V at each timestep.
     x = np.zeros((N,nt))
     v = np.zeros((N,nt))
     V = np.zeros(nt)
@@ -124,7 +155,7 @@ def velocityverlet(N, accel, x0, v0, dt, nt):
     x[:,0] = x0
     v[:,0] = v0
     # NB We only want to call this function once per cycle.
-    a,V[0],r,F = accel(x0)
+    a,V[0] = accel(x0)
 
     # Use a for loop to iterate over timesteps, updating the velocity
     # and position each time.
@@ -135,7 +166,7 @@ def velocityverlet(N, accel, x0, v0, dt, nt):
         # Sometimes you'll see this with the two preceeding steps
         # combined to one, and a full step update of v
         # at the end
-        a,V[it],r,F = accel(x[:,it])
+        a,V[it] = accel(x[:,it])
         # This is basically equivalent to the leapfrog method
         # except that we have v at that timestep at the end of
         # each step.
@@ -145,51 +176,14 @@ def velocityverlet(N, accel, x0, v0, dt, nt):
         # plt.show()
     return x,v,V
 
-def leapfrog(N,accel, x0, v0, dt, nt):
-    '''Leapfrog integration to find x(t) and v(t)
+#sns.palplot(sns.color_palette("GnBu_d"))
+def multiplot(N,x,y,l,cmap,plotname=None):
+    plt.figure(N)
+    sns.set_palette(cmap)
+    for i in range(N):
+        plt.plot(x,y[i]-y[i][0], linewidth = l)
+    plt.savefig(plotname)
 
-    Parameters
-    ----------
-    N : integer
-        Number of atoms in chain
-    accel : function
-        Function of x that gives the acceleration.
-    x0 : float
-        Initial value of x.
-    v0 : float
-        Initial value of v.
-    dt : float
-        Timestep.
-    nt : int
-        Number of timesteps
-
-    Returns
-    -------
-    x : float, array
-        Position at each timestep.
-    '''
-    # Initialize an empty arrays that will store x at each timestep.
-    x = np.empty((N,nt))
-    # Add in the initial conditions.
-    x = np.empty((N,nt))
-    v = np.empty((N,nt))
-    V = np.empty(nt)
-    # Add in the initial conditions.
-    x[:,0] = x0
-    # Put velocity out of sync by half a step (leapfrog method
-    a,V[0],r,F = accel(x0)
-    v[:,0] = v0 + 0.5 * dt * a
-
-    # Use a for loop to iterate over timesteps, updating the velocity
-    # and position each time.
-    for it in range(1, nt):
-        x[:,it] = x[:,it-1] + dt * v[:,it-1]
-        a,V[it],r,F = accel(x[:,it])
-        v[:,it] = v[:,it-1] + dt * a
-
-    # plt.plot(x[:,it], np.zeros(N), 'ro')
-    # plt.show()
-    return x,v,V
 
 def main(N,t,dt):
     '''
@@ -201,33 +195,57 @@ def main(N,t,dt):
     N - Number of argon atoms (<100)
     t - duration of simulation (ps)
     dt - timestep used in integration of equation of motion (ps)
+
     '''
 
+    #Unit conversions
+    e = 1.6e-19
+    A_to_m = 1e-10
+    ps_to_s = 1e-12
+
+    #Alternative units - to SI and back
+    Fev_to_FSI = e/A_to_m #converting a force in eV per Angstrom to kg m s^-2
+    aSI_to_aAps = ps_to_s**2/A_to_m # converting an acceleration from ms^-2 to Angstrom per ps^-2
+
+    m = 6.63e-26/(Fev_to_FSI*aSI_to_aAps) # mass in eV ps^2 per Angstrom^2
+
+    #Initialise time-step and duration, determine initial values and call differential equation solver
     nt = int(t/dt)
     tvals = np.linspace(0, t, nt)
     pos,v0 = Init(N)
     xvals,vvals,Vvals = velocityverlet(N,Accel,pos,v0,dt,nt)
 
-    for i in range(len(xvals)):
-        plt.plot(tvals, xvals[i]-xvals[i][0])
-    plt.savefig('Displacement from initial position',dpi=700)
+    samples = np.zeros(nt)
 
-    Tvals = [0.5*m*np.sum((vvals[:,i])**2) for i in range(len(vvals[0]))]
-    print('V = ', len(Vvals),' ',Vvals[3], 'T = ', len(Tvals),' ',Tvals[3], 't = ', len(tvals))
-    print('V = ', Vvals-Vvals[0])
-    print('T = ', Tvals)
+    #plot displacement from initial position for each atom
+    # for i in range(len(xvals)):
+    #     plt.plot(tvals, xvals[i]-xvals[i][0], linewidth = 0.5)
+    # plt.savefig('Displacement from initial position',dpi=700)
+    multiplot(len(xvals),tvals,xvals,0.5,'GnBu_d','Displacement from initial position')
 
-    plt.close()
-    plt.plot(tvals, Vvals-Vvals[0],'r-.')
-    plt.savefig('Potential Energy')
-    plt.plot(tvals, Tvals,'c-')
-    plt.savefig('Kinetic Energy')
-    plt.close()
-    plt.plot(tvals, Tvals+Vvals, 'b--')
-    plt.show()
+    #Calculate kinetic energy at each timestep as sum of individual atomic kinetic energies
+    Tvals = np.array([0.5*m*np.sum(np.multiply(vvals[:,i],vvals[:,i])) for i in range(nt)])
+    print('V = ', len(Vvals),' ',Vvals-Vvals[0], 'T = ', len(Tvals),' ',Tvals, 't = ', len(tvals))
+
+    #Generate plots of potential, kinetic and total energies
+    # plt.figure(6)
+    # plt.plot(tvals, Vvals-Vvals[0],'r-.',linewidth = 0.2) #Sometimes this is a positive curve and sometimes it is negative.....
+    # #plt.savefig('Potential Energy')
+    # #plt.figure(7)
+    # plt.plot(tvals, Tvals,'c-',linewidth = 0.2) #Tiimes by 10^-2 gives same order of magnitude as V but I don't know why..
+    # #plt.savefig('Kinetic Energy')
+    # #plt.figure(8)
+    # plt.plot(tvals, Tvals+Vvals-Vvals[0], 'b--',linewidth = 0.2)
+    # plt.savefig('Total Energy')
+
+    Energies = [Vvals,Tvals,Vvals+Tvals]
+
+    multiplot(3,tvals,Energies,0.2,'Set1','Energies')
+
+
+    return Tvals+Vvals-Vvals[0]
 
 if __name__=='__main__':
-    pos, v0 = Init(100)
+    #pos, v0 = Init(100)
     #a,V,r,F = Accel(pos)
-    x,v,V = velocityverlet(100, Accel, pos, v0, 0.05, 50)
-    x2,v2,V2 = leapfrog(100, Accel, pos, v0, 0.05, 50)
+    main(2,50,0.05)
